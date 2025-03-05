2ad2c8b346c85aacc581c2ba972f337bcffeeade
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2025-03-05 11:15:02 +0900

    Remove ifdef USE_STRICT_INT (CDocLine)

--- sakura_core/docplus/CBookmarkManager.cpp
+++ sakura_core/docplus/CBookmarkManager.cpp
@@ -212,7 +212,7 @@ void CBookmarkManager::MarkSearchWord(
 	const SSearchOption&	sSearchOption = pattern.GetSearchOption();
 	CDocLine*	pDocLine;
 	const wchar_t*	pLine;
-	int			nLineLen;
+	CLogicInt	nLineLen;
 
 	/* 1==正規表現 */
 	if( sSearchOption.bRegularExp ){
