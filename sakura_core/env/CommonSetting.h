50b941f73c666b98ea70e7b897ce1a38f2da5032
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-05-02 23:09:28 +0900

    assply StaticString (m_szWindowCaptionActive)

--- sakura_core/env/CommonSetting.h
+++ sakura_core/env/CommonSetting.h
@@ -121,8 +121,8 @@ struct CommonSetting_Window
 	BOOL			m_bSplitterWndVScroll;		//!< 分割ウィンドウの垂直スクロールの同期をとる 2001/06/20 asa-o
 
 	//タイトルバー
-	WCHAR			m_szWindowCaptionActive  [MAX_CAPTION_CONF_LEN];	//!< タイトルバー(アクティブ時)
-	WCHAR			m_szWindowCaptionInactive[MAX_CAPTION_CONF_LEN];	//!< タイトルバー(非アクティブ時)
+	StaticString<MAX_CAPTION_CONF_LEN>	m_szWindowCaptionActive;	//!< タイトルバー(アクティブ時)
+	StaticString<MAX_CAPTION_CONF_LEN>	m_szWindowCaptionInactive;	//!< タイトルバー(非アクティブ時)
 
 	//INI内設定のみ
 	int				m_nVertLineOffset;			//!< 縦線の描画座標オフセット 2005.11.10 Moca
