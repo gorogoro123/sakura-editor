06619b7108fb59c2e86a108b20f3d36cdc36c429
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-05-23 20:38:43 +0900

    apply make_unique (CEditApp)

--- sakura_core/CEditApp.cpp
+++ sakura_core/CEditApp.cpp
@@ -21,6 +21,14 @@
 #include "util/module.h"
 #include "util/shell.h"
 
+CEditApp::CEditApp()
+{
+}
+
+CEditApp::~CEditApp()
+{
+}
+
 void CEditApp::Create(HINSTANCE hInst, int nGroupId)
 {
 	m_hInst = hInst;
@@ -29,52 +37,39 @@ void CEditApp::Create(HINSTANCE hInst, int nGroupId)
 	m_cIcons.Create( m_hInst );	//	CreateImage List
 
 	//ドキュメントの作成
-	m_pcEditDoc = new CEditDoc(this);
+	m_pcEditDoc = std::make_unique<CEditDoc>(this);
 
 	//IO管理
-	m_pcLoadAgent = new CLoadAgent();
-	m_pcSaveAgent = new CSaveAgent();
-	m_pcVisualProgress = new CVisualProgress();
+	m_pcLoadAgent = std::make_unique<CLoadAgent>();
+	m_pcSaveAgent = std::make_unique<CSaveAgent>();
+	m_pcVisualProgress = std::make_unique<CVisualProgress>();
 
 	//GREPモード管理
-	m_pcGrepAgent = new CGrepAgent();
+	m_pcGrepAgent = std::make_unique<CGrepAgent>();
 
 	//編集モード
 	CAppMode::getInstance();	//ウィンドウよりも前にイベントを受け取るためにここでインスタンス作成
 
 	//マクロ
-	m_pcSMacroMgr = new CSMacroMgr();
+	m_pcSMacroMgr = std::make_unique<CSMacroMgr>();
 
 	//ドキュメントの作成
 	m_pcEditDoc->Create();
 
 	//ウィンドウの作成
-	m_pcEditWnd = new CEditWnd();
-	m_pcEditWnd->Create( m_pcEditDoc, &m_cIcons, nGroupId );
+	m_pcEditWnd = std::make_unique<CEditWnd>();
+	m_pcEditWnd->Create( m_pcEditDoc.get(), &m_cIcons, nGroupId);
 
 	//MRU管理
-	m_pcMruListener = new CMruListener();
+	m_pcMruListener = std::make_unique<CMruListener>();
 
 	//プロパティ管理
-	m_pcPropertyManager = new CPropertyManager();
+	m_pcPropertyManager = std::make_unique<CPropertyManager>();
 	m_pcPropertyManager->Create(
 		m_pcEditWnd->GetHwnd(),
 		&GetIcons(),
 		&m_pcEditWnd->GetMenuDrawer()
 	);
-}
-
-CEditApp::~CEditApp()
-{
-	delete m_pcSMacroMgr;
-	delete m_pcPropertyManager;
-	delete m_pcMruListener;
-	delete m_pcGrepAgent;
-	delete m_pcVisualProgress;
-	delete m_pcSaveAgent;
-	delete m_pcLoadAgent;
-	delete m_pcEditWnd;
-	delete m_pcEditDoc;
 }
 
 /*! 共通設定 プロパティシート */
