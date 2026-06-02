gorogoro123/sakura

Fix "-Wundefined-bool-conversion" warning

sakura_core/cmd/CViewCommander_Bookmark.cpp
@@ -138,7 +138,7 @@ void CViewCommander::Command_JUMP( void )
 		CLogicInt	nLineLen;
 		CLogicInt	nBgn = CLogicInt(0);
 		CLogicInt	i;
-		pLine = GetDocument()->m_cDocLineMgr.GetLine(CLogicInt(nLineCount))->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL_Safe(GetDocument()->m_cDocLineMgr.GetLine(CLogicInt(nLineCount)), &nLineLen);
 		bValidLine = FALSE;
 		for( i = CLogicInt(0); i < nLineLen; ++i ){
 			if( L' ' != pLine[i] &&

sakura_core/doc/layout/CTsvModeInfo.cpp
@@ -49,7 +49,7 @@ void CTsvModeInfo::CalcTabLength(CDocLineMgr* cDocLineMgr)
 		int nCharChars;
 		int nField = 0;
 		int nFieldWidth = 0;
-		LPCWSTR pcLine = cDocLine->GetDocLineStrWithEOL(&nLineLen);
+		LPCWSTR pcLine = CDocLine::GetDocLineStrWithEOL_Safe( cDocLine, &nLineLen);
 		for (i = 0; i < nLineLen; ) {
 			if (WCODE::IsLineDelimiter(pcLine[i], true)) break;
 			if (pcLine[i] == delimiter) {

sakura_core/types/CType_Cobol.cpp
@@ -73,7 +73,7 @@ void CDocOutline::MakeTopicList_cobol( CFuncInfoArr* pcFuncInfoArr )
 
 	CLogicInt	nLineCount;
 	for( nLineCount = CLogicInt(0); nLineCount <  m_pcDocRef->m_cDocLineMgr.GetLineCount(); ++nLineCount ){
-		pLine = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL_Safe( m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 		if( NULL == pLine ){
 			break;
 		}

sakura_core/types/CType_Cpp.cpp
@@ -519,7 +519,7 @@ void CDocOutline::MakeFuncList_C( CFuncInfoArr* pcFuncInfoArr ,EOutlineType& nOu
 	
 	CLogicInt		nLineCount;
 	for( nLineCount = CLogicInt(0); nLineCount <  m_pcDocRef->m_cDocLineMgr.GetLineCount(); ++nLineCount ){
-		pLine = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL_Safe( m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 
 		//	From Here Aug. 10, 2004 genta
 		//	プリプロセス処理
@@ -1358,7 +1358,7 @@ void CEditView::SmartIndent_CPP( wchar_t wcChar )
 		wchar_t wcCharOrg = wcChar;
 		nCaretPosX_PHY = GetCaret().GetCaretLogicPos().x;
 
-		pLine = m_pcEditDoc->m_cDocLineMgr.GetLine(GetCaret().GetCaretLogicPos().GetY2())->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL_Safe( m_pcEditDoc->m_cDocLineMgr.GetLine(GetCaret().GetCaretLogicPos().GetY2()), &nLineLen);
 		if( NULL == pLine ){
 			if( WCODE::CR != wcChar ){
 				return;
@@ -1426,7 +1426,7 @@ void CEditView::SmartIndent_CPP( wchar_t wcChar )
 
 		nDataLen = CLogicInt(0);
 		for( j = GetCaret().GetCaretLogicPos().GetY2(); j >= CLogicInt(0); --j ){
-			pLine2 = m_pcEditDoc->m_cDocLineMgr.GetLine(j)->GetDocLineStrWithEOL(&nLineLen2);
+			pLine2 = CDocLine::GetDocLineStrWithEOL_Safe( m_pcEditDoc->m_cDocLineMgr.GetLine(j), &nLineLen2);
 			if( j == GetCaret().GetCaretLogicPos().y ){
 				// 2005.10.11 ryoji EOF のみの行もスマートインデントの対象にする
 				if( NULL == pLine2 ){

sakura_core/types/CType_Html.cpp
@@ -99,7 +99,7 @@ void CDocOutline::MakeTopicList_html(CFuncInfoArr* pcFuncInfoArr, bool bXml)
 	}
 	for(nLineCount=CLogicInt(0);nLineCount<m_pcDocRef->m_cDocLineMgr.GetLineCount();nLineCount++)
 	{
-		pLineBuf	=	m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLineBuf = CDocLine::GetDocLineStrWithEOL_Safe( m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 		if( !pLineBuf )
 		{
 			break;
@@ -306,7 +306,7 @@ void CDocOutline::MakeTopicList_html(CFuncInfoArr* pcFuncInfoArr, bool bXml)
 								}
 								if (bLoop) {
 									nLineCount++;
-									pLineBuf = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+									pLineBuf = CDocLine::GetDocLineStrWithEOL_Safe( m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 									pLine = pLineBuf;
 									j = 0;
 									i = 0;

sakura_core/types/CType_Python.cpp
@@ -405,7 +405,7 @@ void CDocOutline::MakeFuncList_python( CFuncInfoArr* pcFuncInfoArr )
 		int depth;	//	indent depth
 		CLogicInt col = CLogicInt(0);	//	current working column position
 
-		pLine = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL_Safe( m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 		
 		if( python_analyze_state.IsLogicalLineTop() ){
 			//	indent check

sakura_core/types/CType_Tex.cpp
@@ -228,7 +228,7 @@ public:
 	{
 		const CLogicInt nLineCount = refDocLineMgr.GetLineCount();
 		for (CLogicInt nLineLen, nLineNumber = CLogicInt(0); nLineNumber < nLineCount; ++nLineNumber) {
-			const wchar_t* const pLine = refDocLineMgr.GetLine(nLineNumber)->GetDocLineStrWithEOL(&nLineLen);
+			const wchar_t* const pLine = CDocLine::GetDocLineStrWithEOL_Safe( refDocLineMgr.GetLine(nLineNumber), &nLineLen);
 			const wchar_t* const pLineEnd = pLine + nLineLen;
 			if (! pLine) { // [EOF] のみの行。
 				break;

gorogoro123/sakura-editor

sakura_core/cmd/CViewCommander_Bookmark.cpp
@@ -136,7 +136,7 @@ void CViewCommander::Command_JUMP( )
 		CLogicInt	nLineLen;
 		CLogicInt	nBgn = CLogicInt(0);
 		CLogicInt	i;
-		pLine = GetDocument()->m_cDocLineMgr.GetLine(CLogicInt(nLineCount))->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL(GetDocument()->m_cDocLineMgr.GetLine(CLogicInt(nLineCount)), &nLineLen);
 		bValidLine = FALSE;
 		for( i = CLogicInt(0); i < nLineLen; ++i ){
 			if( L' ' != pLine[i] &&

sakura_core/doc/layout/CTsvModeInfo.cpp
@@ -29,7 +29,7 @@ void CTsvModeInfo::CalcTabLength(CDocLineMgr* cDocLineMgr)
 		int nCharChars;
 		int nField = 0;
 		int nFieldWidth = 0;
-		LPCWSTR pcLine = cDocLine->GetDocLineStrWithEOL(&nLineLen);
+		LPCWSTR pcLine = CDocLine::GetDocLineStrWithEOL(cDocLine, &nLineLen);
 		for (i = 0; i < nLineLen; ) {
 			if (WCODE::IsLineDelimiter(pcLine[i], true)) break;
 			if (pcLine[i] == delimiter) {

sakura_core/types/CType_Cobol.cpp
@@ -54,7 +54,7 @@ void CDocOutline::MakeTopicList_cobol( CFuncInfoArr* pcFuncInfoArr )
 
 	CLogicInt	nLineCount;
 	for( nLineCount = CLogicInt(0); nLineCount <  m_pcDocRef->m_cDocLineMgr.GetLineCount(); ++nLineCount ){
-		pLine = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL(m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 		if( nullptr == pLine ){
 			break;
 		}

sakura_core/types/CType_Cpp.cpp
@@ -499,7 +499,7 @@ void CDocOutline::MakeFuncList_C( CFuncInfoArr* pcFuncInfoArr ,EOutlineType& nOu
 	
 	CLogicInt		nLineCount;
 	for( nLineCount = CLogicInt(0); nLineCount <  m_pcDocRef->m_cDocLineMgr.GetLineCount(); ++nLineCount ){
-		pLine = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL(m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 
 		//	From Here Aug. 10, 2004 genta
 		//	プリプロセス処理
@@ -1338,7 +1338,7 @@ void CEditView::SmartIndent_CPP( wchar_t wcChar )
 		wchar_t wcCharOrg = wcChar;
 		nCaretPosX_PHY = GetCaret().GetCaretLogicPos().x;
 
-		pLine = m_pcEditDoc->m_cDocLineMgr.GetLine(GetCaret().GetCaretLogicPos().GetY2())->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL(m_pcEditDoc->m_cDocLineMgr.GetLine(GetCaret().GetCaretLogicPos().GetY2()), &nLineLen);
 		if( nullptr == pLine ){
 			if( WCODE::CR != wcChar ){
 				return;
@@ -1406,7 +1406,7 @@ void CEditView::SmartIndent_CPP( wchar_t wcChar )
 
 		nDataLen = CLogicInt(0);
 		for( j = GetCaret().GetCaretLogicPos().GetY2(); j >= CLogicInt(0); --j ){
-			pLine2 = m_pcEditDoc->m_cDocLineMgr.GetLine(j)->GetDocLineStrWithEOL(&nLineLen2);
+			pLine2 = CDocLine::GetDocLineStrWithEOL(m_pcEditDoc->m_cDocLineMgr.GetLine(j), &nLineLen2);
 			if( j == GetCaret().GetCaretLogicPos().y ){
 				// 2005.10.11 ryoji EOF のみの行もスマートインデントの対象にする
 				if( nullptr == pLine2 ){

sakura_core/types/CType_Html.cpp
@@ -80,7 +80,7 @@ void CDocOutline::MakeTopicList_html(CFuncInfoArr* pcFuncInfoArr, bool bXml)
 	}
 	for(nLineCount=CLogicInt(0);nLineCount<m_pcDocRef->m_cDocLineMgr.GetLineCount();nLineCount++)
 	{
-		pLineBuf	=	m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLineBuf = CDocLine::GetDocLineStrWithEOL(m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 		if( !pLineBuf )
 		{
 			break;
@@ -287,7 +287,7 @@ void CDocOutline::MakeTopicList_html(CFuncInfoArr* pcFuncInfoArr, bool bXml)
 								}
 								if (bLoop) {
 									nLineCount++;
-									pLineBuf = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+									pLineBuf = CDocLine::GetDocLineStrWithEOL(m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 									pLine = pLineBuf;
 									j = 0;
 									i = 0;

sakura_core/types/CType_Python.cpp
@@ -385,7 +385,7 @@ void CDocOutline::MakeFuncList_python( CFuncInfoArr* pcFuncInfoArr )
 		int depth;	//	indent depth
 		CLogicInt col = CLogicInt(0);	//	current working column position
 
-		pLine = m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount)->GetDocLineStrWithEOL(&nLineLen);
+		pLine = CDocLine::GetDocLineStrWithEOL(m_pcDocRef->m_cDocLineMgr.GetLine(nLineCount), &nLineLen);
 		
 		if( python_analyze_state.IsLogicalLineTop() ){
 			//	indent check

sakura_core/types/CType_Tex.cpp
@@ -205,7 +205,7 @@ public:
 	{
 		const CLogicInt nLineCount = refDocLineMgr.GetLineCount();
 		for (CLogicInt nLineLen, nLineNumber = CLogicInt(0); nLineNumber < nLineCount; ++nLineNumber) {
-			const wchar_t* const pLine = refDocLineMgr.GetLine(nLineNumber)->GetDocLineStrWithEOL(&nLineLen);
+			const wchar_t* const pLine = CDocLine::GetDocLineStrWithEOL(refDocLineMgr.GetLine(nLineNumber), &nLineLen);
 			const wchar_t* const pLineEnd = pLine + nLineLen;
 			if (! pLine) { // [EOF] のみの行。
 				break;