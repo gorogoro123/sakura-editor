gorogoro123/sakura

Remove SelectDir function template

sakura_core/dlg/CDlgExec.cpp
@@ -262,7 +262,9 @@ BOOL CDlgExec::OnBnClicked( int wID )
 
 	case IDC_BUTTON_REFERENCE2:
 		{
-			if( SelectDir( GetHwnd(), LS(STR_DLGEXEC_SELECT_CURDIR), &m_szCurDir[0], &m_szCurDir[0], m_szCurDir.GetBufferCount() ) ){
+			std::wstring szTmpPath;
+			if( SelectDir( GetHwnd(), LS(STR_DLGEXEC_SELECT_CURDIR), m_szCurDir.c_str(), szTmpPath ) ){
+				wcsncpy_s( m_szCurDir, _countof2(m_szCurDir), szTmpPath.c_str(), _TRUNCATE);
 				::DlgItem_SetText( GetHwnd(), IDC_COMBO_CUR_DIR, &m_szCurDir[0] );
 			}
 		}

gorogoro123/sakura-editor

sakura_core/dlg/CDlgExec.cpp
@@ -250,7 +250,7 @@ BOOL CDlgExec::OnBnClicked( int wID )
 
 	case IDC_BUTTON_REFERENCE2:
 		{
-			if( SelectDir( GetHwnd(), LS(STR_DLGEXEC_SELECT_CURDIR), &m_szCurDir[0], &m_szCurDir[0], m_szCurDir.capacity() ) ){
+			if( SelectDir( GetHwnd(), LS(STR_DLGEXEC_SELECT_CURDIR), m_szCurDir.c_str(), m_szCurDir ) ){
 				ApiWrap::DlgItem_SetText( GetHwnd(), IDC_COMBO_CUR_DIR, &m_szCurDir[0] );
 			}
 		}