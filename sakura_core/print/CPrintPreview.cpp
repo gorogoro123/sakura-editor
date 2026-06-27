331f6dbea7667c38be466d2425a940e11f2ee17f
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-06-27 18:35:04 +0900

    replace _itow

--- sakura_core/print/CPrintPreview.cpp
+++ sakura_core/print/CPrintPreview.cpp
@@ -1460,12 +1460,12 @@ CColorStrategy* CPrintPreview::DrawPageText(
 					if( 0 != pcLayout->GetLogicOffset() ){ //折り返しレイアウト行
 						wcscpy( szLineNum, L" " );
 					}else{
-						_itow( pcLayout->GetLogicLineNo() + 1, szLineNum, 10 );	/* 対応する論理行番号 */
+						_itow_s( pcLayout->GetLogicLineNo() + 1, szLineNum, 10 );	/* 対応する論理行番号 */
 					}
 				}
 				else{
 					/* 物理行(レイアウト行)番号表示モード */
-					_itow( (Int)nLineNum + 1, szLineNum, 10 );
+					_itow_s( (int)nLineNum + 1, szLineNum, 10 );
 				}
 
 				/* 行番号区切り  0=なし 1=縦線 2=任意 */
