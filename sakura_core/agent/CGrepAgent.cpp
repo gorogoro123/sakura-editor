311ec1e008b3ed1e063d37ef0680daff81ea318f
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-07-02 20:07:46 +0900

    replace wcscpy (CGrepAgent)

--- sakura_core/agent/CGrepAgent.cpp
+++ sakura_core/agent/CGrepAgent.cpp
@@ -1418,7 +1418,7 @@ int CGrepAgent::DoGrepFile(
 		{
 			if( CODE_AUTODETECT == sGrepOption.nGrepCharSet ){
 				if( IsValidCodeType(nCharCode) ){
-					wcscpy( szCpName, CCodeTypeName(nCharCode).Bracket() );
+					wcscpy_s( szCpName, std::size(szCpName), CCodeTypeName(nCharCode).Bracket() );
 					pszCodeName = szCpName;
 				}else{
 					CCodePage::GetNameBracket(szCpName, nCharCode);
@@ -1865,7 +1865,7 @@ int CGrepAgent::DoGrepReplaceFile(
 		{
 			if( CODE_AUTODETECT == sGrepOption.nGrepCharSet ){
 				if( IsValidCodeType(nCharCode) ){
-					wcscpy( szCpName, CCodeTypeName(nCharCode).Bracket() );
+					wcscpy_s( szCpName, std::size(szCpName), CCodeTypeName(nCharCode).Bracket() );
 					pszCodeName = szCpName;
 				}else{
 					CCodePage::GetNameBracket(szCpName, nCharCode);
