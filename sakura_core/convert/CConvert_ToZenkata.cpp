d5bcc6486144b85872751237e38cbf43bec82f7e
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-07-25 08:09:11 +0900

    replace new (CConvert_ToZenkata)

--- sakura_core/convert/CConvert_ToZenkata.cpp
+++ sakura_core/convert/CConvert_ToZenkata.cpp
@@ -16,21 +16,18 @@
 bool CConvert_ToZenkata::DoConvert(CNativeW* pcData)
 {
 	//半角カナ→全角カナ
-	wchar_t* pBuf = new wchar_t[pcData->GetStringLength()+1]; //文字数が減ることはあっても増えることは無いので、これでＯＫ
+	std::vector<wchar_t> vBuf(static_cast<size_t>(pcData->GetStringLength())+1); //文字数が減ることはあっても増えることは無いので、これでＯＫ
 	int nBufLen = 0;
-	Convert_HankataToZenkata(pcData->GetStringPtr(), pcData->GetStringLength(), pBuf, &nBufLen);
+	Convert_HankataToZenkata(pcData->GetStringPtr(), pcData->GetStringLength(), vBuf.data(), &nBufLen);
 
 	//全角ひら→全角カナ
-	Convert_ZenhiraToZenkata(pBuf, nBufLen);
+	Convert_ZenhiraToZenkata(vBuf.data(), nBufLen);
 
 	//半角英数→全角英数
-	Convert_HaneisuToZeneisu(pBuf, nBufLen);
+	Convert_HaneisuToZeneisu(vBuf.data(), nBufLen);
 
 	//設定
-	pcData->SetString(pBuf, nBufLen);
-
-	//バッファ解放
-	delete[] pBuf;
+	pcData->SetString(vBuf.data(), nBufLen);
 
 	return true;
 }
