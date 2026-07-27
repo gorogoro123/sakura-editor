dd81d0edd4da903d3ee6d360dd3c743fbecbb37a
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-07-27 21:08:40 +0900

    replacw CStringRef (RegexIsKeyword)

--- sakura_core/env/CRegexKeyword.cpp
+++ sakura_core/env/CRegexKeyword.cpp
@@ -340,7 +340,7 @@ BOOL CRegexKeyword::RegexKeyLineStart( )
 	@note RegexKeyLineStart関数によって初期化されていること。
 */
 BOOL CRegexKeyword::RegexIsKeyword(
-	const CStringRef&	cStr,		//!< [in] 検索対象文字列
+	std::span<const WCHAR> cStr,	//!< [in] 検索対象文字列
 //	const wchar_t*		pLine,		//!< [in] １行のデータ
 	int					nPos,		//!< [in] 検索開始オフセット
 //	int					nLineLen,	//!< [in] １行の長さ
