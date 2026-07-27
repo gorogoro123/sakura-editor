94d303dac3e9e938f31686285d7eb5d95c5a2ab7
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-07-28 06:26:58 +0900

    modernize CStringRef

--- sakura_core/mem/CNativeW.h
+++ sakura_core/mem/CNativeW.h
@@ -27,6 +27,13 @@ public:
 	[[nodiscard]] wchar_t At( size_t nIndex ) const noexcept;
 	[[nodiscard]] wchar_t operator []( size_t nIndex ) const noexcept { return m_pData[nIndex]; }
 
+	[[nodiscard]] constexpr const wchar_t* data() const noexcept { return m_pData; }
+	[[nodiscard]] constexpr size_t size() const noexcept { return m_nDataLen; }
+	[[nodiscard]] constexpr bool empty() const noexcept { return m_nDataLen == 0; }
+	[[nodiscard]] operator std::span<const wchar_t>() const noexcept {
+		return std::span<const wchar_t>(m_pData, m_nDataLen);
+	}
+
 private:
 	const wchar_t*	m_pData = nullptr;
 	unsigned		m_nDataLen = 0;
