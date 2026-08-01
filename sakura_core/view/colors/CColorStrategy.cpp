e8f893cf262e8e6af98038533351126d34f36e21
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-08-01 20:13:53 +0900

    apply std::span (IsPosKeywordHead)

--- sakura_core/view/colors/CColorStrategy.cpp
+++ sakura_core/view/colors/CColorStrategy.cpp
@@ -21,9 +21,9 @@
 #include "window/CEditWnd.h"
 #include "types/CTypeSupport.h"
 
-bool _IsPosKeywordHead(const CStringRef& cStr, int nPos)
+bool _IsPosKeywordHead(std::span<const WCHAR> cStr, int nPos)
 {
-	return (nPos==0 || !IS_KEYWORD_CHAR(cStr.At(nPos-1)));
+	return (nPos==0 || (nPos < cStr.size() && !IS_KEYWORD_CHAR(cStr[nPos-1])));
 }
 
 /*! 色の切り替え判定
