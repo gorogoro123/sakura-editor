012a5b51283d4930ee80e31058ae9e195c9c529c
gorogoro123 <897627+gorogoro123@users.noreply.github.com>
2026-09-23 20:34:14 +0900

    replace swscanf (ShareData_IO_2)

--- sakura_core/env/CShareData_IO.cpp
+++ sakura_core/env/CShareData_IO.cpp
@@ -110,11 +110,14 @@ bool CShareData_IO::ShareData_IO_2( bool bRead )
 		}
 
 		// バージョンアップ時はバックアップファイルを作成する	// 2011.01.28 ryoji
-		WCHAR iniVer[256];
-		DWORD mH, mL, lH, lL;
-		mH = mL = lH = lL = 0;	// ※ 古～い ini だと "szVersion" は無い
-		if( cProfile.IOProfileData(L"Other", L"szVersion", StringBufferW(iniVer)) )
-			swscanf( iniVer, L"%u.%u.%u.%u", &mH, &mL, &lH, &lL );
+		StaticString<256> iniVer;
+		DWORD mH = 0; // ※ 古～い ini だと "szVersion" は無い
+		DWORD mL = 0;
+		DWORD lH = 0;
+		DWORD lL = 0;
+		if( cProfile.IOProfileData(L"Other", L"szVersion", iniVer) ) {
+			swscanf_s( iniVer, L"%u.%u.%u.%u", &mH, &mL, &lH, &lL );
+		}
 		DWORD dwMS = (DWORD)MAKELONG(mL, mH);
 		DWORD dwLS = (DWORD)MAKELONG(lL, lH);
 		DLLSHAREDATA* pShareData = &GetDllShareData();
